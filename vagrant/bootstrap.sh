093ef9869bb5576f729deec1503f409366e6000c
Dmitry Krasun <krasun.net@gmail.com>
2014-10-05 04:58:00 +0300

    Add copying parameters

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,3 +5,4 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 cd /home/vagrant/battle-balancer
 composer install
+cp config/parameters.php.dist config/parameters.php
